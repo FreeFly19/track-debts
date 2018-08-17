1654ee773cf8d0773e782bafb43ca60f96f9dc9c
FreeFly19 <oleksandr.melnyk19@gmail.com>
2018-08-17 22:18:31 +0300

    Init commit

---
+++ src/main/java/com/freefly19/trackdebts/TrackDebtsApplication.java
@@ -0,0 +1,12 @@
+package com.freefly19.trackdebts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackDebtsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackDebtsApplication.class, args);
+	}
+}
