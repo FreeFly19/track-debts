a4fc3d089da911e98c1e251a83cd25129e14ec46
FreeFly19 <oleksandr.melnyk19@gmail.com>
2018-08-17 23:55:22 +0300

    Add validation to user registration API. So all are passed

--- src/main/java/com/freefly19/trackdebts/user/RegisterUserCommand.java
+++ src/main/java/com/freefly19/trackdebts/user/RegisterUserCommand.java
@@ -2,8 +2,13 @@ package com.freefly19.trackdebts.user;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+
 @Data
 class RegisterUserCommand {
+    @NotBlank
     private String email;
+
+    @NotBlank
     private String password;
 }
--- src/main/java/com/freefly19/trackdebts/user/UserController.java
+++ src/main/java/com/freefly19/trackdebts/user/UserController.java
@@ -4,10 +4,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+
 @RestController
 public class UserController {
     @PostMapping("/users")
-    UserDto register(@RequestBody RegisterUserCommand command) {
+    UserDto register(@RequestBody @Valid RegisterUserCommand command) {
         return new UserDto(1, command.getEmail());
     }
 
