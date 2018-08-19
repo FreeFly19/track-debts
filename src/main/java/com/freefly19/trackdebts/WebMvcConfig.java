7d95c6ce2d3d93cc783d7965f1946b20049a4810
FreeFly19 <oleksandr.melnyk19@gmail.com>
2018-08-19 17:20:36 +0300

    Exclude Error controller from swagger

--- src/main/java/com/freefly19/trackdebts/WebMvcConfig.java
+++ src/main/java/com/freefly19/trackdebts/WebMvcConfig.java
@@ -1,17 +1,15 @@
 package com.freefly19.trackdebts;
 
+import com.google.common.base.Predicates;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ResponseMessage;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import java.util.Arrays;
 
 @Configuration
 @EnableSwagger2
@@ -21,7 +19,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(Predicates.not(RequestHandlerSelectors.basePackage("org.springframework.boot")))
                 .paths(PathSelectors.any())
                 .build();
     }
