d2dd9dc1809a8426329fc7fe70327f88ecbd1042
FreeFly19 <oleksandr.melnyk19@gmail.com>
2018-09-11 00:55:00 +0300

    Fix bull lock handler. So it will collapse all transaction by users within one bill

--- src/main/java/com/freefly19/trackdebts/BillLockToMoneyTransactionHandler.java
+++ src/main/java/com/freefly19/trackdebts/BillLockToMoneyTransactionHandler.java
@@ -6,42 +6,56 @@ import com.freefly19.trackdebts.bill.item.participant.ItemParticipant;
 import com.freefly19.trackdebts.bill.lock.BillLockedEvent;
 import com.freefly19.trackdebts.moneytransaction.MoneyTransaction;
 import com.freefly19.trackdebts.moneytransaction.MoneyTransactionRepository;
+import com.freefly19.trackdebts.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
-import java.util.stream.Collectors;
+import java.util.HashMap;
 
 @RequiredArgsConstructor
 @Service
 public class BillLockToMoneyTransactionHandler {
     private final BillRepository billRepository;
+    private final UserRepository userRepository;
     private final MoneyTransactionRepository moneyTransactionRepository;
 
     @EventListener
     public void onBillLocked(BillLockedEvent event) {
         Bill bill = billRepository.getOne(event.getBillId());
 
+        HashMap<Long, BigDecimal> userIdAmountMap = new HashMap<>();
+
         bill.getItems().stream()
                 .flatMap(billItem -> billItem.getParticipants().stream())
                 .filter(p -> !p.getUser().getId().equals(event.getContext().getId()))
-                .map(p -> {
+                .forEach(p -> {
                     BigDecimal coefficientSum = BigDecimal.ZERO;
 
-                    for (ItemParticipant participant: p.getItem().getParticipants()) {
+                    for (ItemParticipant participant : p.getItem().getParticipants()) {
                         coefficientSum = coefficientSum.add(participant.getCoefficient());
                     }
 
-                    return MoneyTransaction.builder()
-                            .sender(p.getUser())
-                            .receiver(p.getItem().getBill().getCreatedBy())
-                            .amount(p.getItem().getCost().multiply(p.getCoefficient().divide(coefficientSum, RoundingMode.HALF_UP)))
+                    BigDecimal amount = userIdAmountMap.getOrDefault(p.getUser().getId(), new BigDecimal(0));
+
+                    BigDecimal amountPerItem = p.getItem().getCost()
+                            .multiply(p.getCoefficient().divide(coefficientSum, 5, RoundingMode.HALF_UP));
+
+                    userIdAmountMap.put(p.getUser().getId(), amount.add(amountPerItem));
+                });
+
+        userIdAmountMap.forEach((userId, amount) -> {
+            moneyTransactionRepository.save(
+                    MoneyTransaction.builder()
+                            .sender(userRepository.getOne(userId))
+                            .receiver(bill.getCreatedBy())
+                            .amount(amount)
                             .createdAt(event.getContext().timestamp())
                             .bill(bill)
-                            .build();
-                })
-                .forEach(moneyTransactionRepository::save);
+                            .build()
+            );
+        });
     }
 }
